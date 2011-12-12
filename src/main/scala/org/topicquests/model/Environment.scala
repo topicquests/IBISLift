b612249a7cb8d888faefa00fc9326747b392d2b8
Jack Park <jackpark@gmail.com>
2011-12-11 22:54:00 -0800

    first commit

---
+++ src/main/scala/org/topicquests/model/Environment.scala
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.topicquests.model
+import net.liftweb._
+import util._
+import Helpers._
+
+/**
+ * @author park
+ *
+ */
+object Environment {
+  var _landingTitle:String = ""
+  var _subTitle:String = "Public, Open signup"
+    
+  def init() {
+    _landingTitle = Props.get("landing.title") openOr "Conversations that matter"
+    var pvt:Boolean = if(Props.get("authentication.required").open_! == "true") true else false
+    var invite = if(Props.get("invite.required").open_! == "true") true else false
+    
+    if (pvt && invite)
+      _subTitle = "Private, Invitation only"
+    else if (!pvt && invite)
+      _subTitle = "Public, Invitation only"
+    else if (pvt && !invite)
+      _subTitle = "Private, Open signup"
+        
+  }
+
+  def landingTitle = _landingTitle
+  def subTitle = _subTitle
+  
+}
